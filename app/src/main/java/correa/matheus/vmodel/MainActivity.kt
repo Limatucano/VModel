21f2d75c4105275184e84775b95c82ffac9806b5
Matheus Lima <matheusmt31@gmail.com>
2021-08-11 20:47:41 -0300

    regra de negocio junto com interface

--- app/src/main/java/correa/matheus/vmodel/MainActivity.kt
+++ app/src/main/java/correa/matheus/vmodel/MainActivity.kt
@@ -2,10 +2,47 @@ package correa.matheus.vmodel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var txtValor : EditText
+    lateinit var btnDados : Button
+    lateinit var btnMostrar : Button
+
+    var contador: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initDados()
+        initContador()
+        initClick()
+        validaContador()
+    }
+    fun validaContador(){
+        if(contador > 5){
+            contador = 0
+        }
+    }
+    fun initClick(){
+        btnDados.setOnClickListener {
+            contador++
+            validaContador()
+            initContador()
+        }
+        btnMostrar.setOnClickListener {
+            Toast.makeText(this,"Valor $contador", Toast.LENGTH_SHORT).show()
+        }
+    }
+    fun initContador(){
+        txtValor.setText(contador.toString())
+    }
+    fun initDados(){
+        txtValor = findViewById(R.id.valor)
+        btnDados = findViewById(R.id.dados)
+        btnMostrar = findViewById(R.id.mostrar)
     }
 }
